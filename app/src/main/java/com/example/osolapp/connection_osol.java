b62f86ecfd1781eafb0ade47cd652a2d337da4c4
alexprime100 <alexprime100@gmail.com>
2020-02-03 16:27:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/osolapp/connection_osol.java
@@ -0,0 +1,14 @@
+package com.example.osolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class connection_osol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_osol);
+    }
+}
---
+++ app/src/main/java/com/example/osolapp/info_pico_user.java
@@ -0,0 +1,14 @@
+package com.example.osolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class info_pico_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_pico_user);
+    }
+}
