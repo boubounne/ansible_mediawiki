62c16f08db63269a6c048d4f3611789a8a745ffe
boubounne <vincentgalasso@yahoo.fr>
2020-03-02 17:05:34 +0100

    1.3.1

---
+++ app/src/main/java/com/example/osolapp/AccesHTTP.java
@@ -0,0 +1,20 @@
+package com.example.osolapp;
+
+import android.os.AsyncTask;
+import org.apache.http.NameValuePair;
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+
+public class AccesHTTP extends AsyncTask<String, Integer,Long>{
+
+    private ArrayList<NameValuePair> parametres;
+
+
+
+    @Override
+    protected Long doInBackground(String... strings){
+        return null;
+    }
+}
